9eb202f52d354c0434f6a92b2299296726d9f982
Isaac Oliveira <92345049+IsaacOliveiraSouza@users.noreply.github.com>
2023-06-10 16:34:53 -0300

    Delete codigos_tcc directory

--- codigos_tcc/controleSupervisor.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include "conexaoBD.php";
-    $sup = new Banco;
-    $email = $_GET['email'];
-    $telefone = $_GET['telefone'];
-    $nome = $_GET['nome'];
-    $cargo = $_GET['cargo'];
-    $novo = $sup->inserirSupervisor($nome, $email, $telefone, $cargo);
-?>
